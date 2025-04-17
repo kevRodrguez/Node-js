9d9811db201ad8d835a52113a09de594c8862be6
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2025-04-17 11:15:45 -0600

    continuacion user store para carga multiple

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import { Authroutes } from './auth/routes';
+import { CategoryRoutes } from './category/routes';
+import { ProductRoutes } from './products/routes';
+
 
 
 
@@ -11,7 +15,9 @@ export class AppRoutes {
     const router = Router();
     
     // Definir las rutas
-    // router.use('/api/todos', /*TodoRoutes.routes */ );
+    router.use('/api/auth', Authroutes.routes );
+    router.use('/api/categories', CategoryRoutes.routes );
+    router.use('/api/products', ProductRoutes.routes );
 
 
 
